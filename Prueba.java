f2fb4322f355d9ef2e2def0ca82e34bdbac0e623
Alumno <danielgalcol@gmail.com>
2023-10-11 14:11:41 +0200

    4ºcommit/ apellidos eliminados y en su lugar hay mi cantante favorito

--- Prueba.java
+++ Prueba.java
@@ -2,8 +2,8 @@ public class Prueba{
 	
 	public static void main(String arg[]) {
 
-	System.out.println("Galende Colino");
 	System.out.println("Tengo 19 años y naci en 2004");
 	System.out.println("La pasta es mi comida favorita");
+	System.out.println("NEFFEX es mi artista favorito"); 
 	}
 }
